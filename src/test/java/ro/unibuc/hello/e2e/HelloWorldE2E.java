bd50eb4387ca67ccc6d31ebd630a5b68272611ef
Erol Cherim <erol.cherim@gmail.com>
2023-05-02 12:18:58 -0400

    [TEST] Added missing e2e tag to test

--- src/test/java/ro/unibuc/hello/e2e/HelloWorldE2E.java
+++ src/test/java/ro/unibuc/hello/e2e/HelloWorldE2E.java
@@ -2,9 +2,11 @@ package ro.unibuc.hello.e2e;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.Tag;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources")
+@Tag("E2E")
 public class HelloWorldE2E {
 }
